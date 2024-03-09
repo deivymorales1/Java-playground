4b3a78e4bf7a9e1e9e1cbd4df62d44350e136e7c
deivy <130388892+deivymorales1@users.noreply.github.com>
2024-03-09 13:57:53 -0500

    :pencil2: Sobrecarga de funciones

--- Java1/src/Funciones.java
+++ Java1/src/Funciones.java
@@ -3,17 +3,20 @@ public class Funciones {
     public static void main(String[] args){
         
         
-        System.out.println(sumar(4 ,4)); // argumentos
-        System.out.println(saludar("Deivy", 27));
+        System.out.println(sumar(4, 4));
+        //System.out.println(saludar("Deivy", 27));
     }
-    
+    // Funcion
     static int sumar(int a, int b){ // Parametros
         return a + b;
     }
     
+        static int sumar(int a, int b){ // Parametros
+        return a + b;
+    }
     
-    // Funciones
-    static String saludar(String nombre, int edad){
-        return "su nombre es " + nombre + " y su edad es: " + edad;
+    
+    static double sumar(double a, double b){
+        return a + b;
     }
 }
