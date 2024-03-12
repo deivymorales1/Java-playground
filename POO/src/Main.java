3102db172c56fa5b859b06ca7e87f678e5c49974
deivy <130388892+deivymorales1@users.noreply.github.com>
2024-03-11 20:12:38 -0500

    :pencil2: Variables y metodo estaticos

--- POO/src/Main.java
+++ POO/src/Main.java
@@ -1,4 +1,6 @@
-import clases.Persona;
+
+import clases.Calculadora;
+import static java.lang.Math.*;
 
 // Entry point
 public class Main {
@@ -7,16 +9,25 @@ public class Main {
     public static void main(String[] args) {
         
         // Instanciamos la clase persona
-        Persona persona1 = new Persona();
-        persona1.nombre = "Deivy";
-        persona1.edad = 27;
-        persona1.mostrarDatos();
+        //Persona persona1 = new Persona();
+        //persona1.nombre = "Deivy";
+        //persona1.edad = 27;
+        //persona1.mostrarDatos();
         
-        Persona persona2 = new Persona();
-        persona2.nombre = "Neo";
-        persona2.edad = 54;
-        persona2.mostrarDatos();
+        //Persona persona2 = new Persona();
+        //persona2.nombre = "Neo";
+        //persona2.edad = 54;
+        //persona2.mostrarDatos();
         
+        
+        System.out.println(Math.PI);
+        System.out.println(Calculadora.PI);
+        Calculadora.PI = 3.15;
+        System.out.println(Calculadora.PI);
+
+        System.out.println(Calculadora.sumar(40, 20));
+        
+        System.out.println(PI);
         
     }
     
---
+++ POO/src/clases/Calculadora.java
@@ -0,0 +1,12 @@
+
+package clases;
+
+
+public class Calculadora {
+    public static double PI = 3.151592;
+    
+    // Metodo estatico
+    public static int sumar(int a, int b){
+        return a + b;
+    }
+}
