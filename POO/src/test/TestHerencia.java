5ae247e73c9dfea5e2285ccabfccff21e7915bca
deivy <130388892+deivymorales1@users.noreply.github.com>
2024-03-11 21:50:15 -0500

    :pencil2: Superclase y protected

---
+++ POO/src/test/TestHerencia.java
@@ -0,0 +1,10 @@
+
+package test;
+import herencia.Persona;
+
+public class TestHerencia {
+    public static void main(String[] args){
+        Persona persona1 = new Persona("Deivy", 'M', 27, "Helechales");
+        System.out.println(persona1);
+    }
+}
